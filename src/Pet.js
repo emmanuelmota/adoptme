14c0963eb590945e8ce874345fb5e4023f6c0b23
Emmanuel Mota <emmanuelmota@gmail.com>
2021-08-10 18:16:04 -0700

    Refactored App and Pet.js files to prepare for jsx usage.

--- src/Pet.js
+++ src/Pet.js
@@ -1,10 +1,20 @@
 import React from 'react';
-const Pet = () => {
-  return React.createElement("div", {}, [
-React.createElement("h2", {}, "Nacho"),
-React.createElement("h3", {}, "Krankers"),
-React.createElement("h3", {}, "Poochi"),
-  ])
+// const Pet = () => {
+//   return React.createElement("div", {}, [
+// React.createElement("h2", {}, "Nacho"),
+// React.createElement("h3", {}, "Krankers"),
+// React.createElement("h3", {}, "Poochi"),
+//   ])
+// }
+
+const Pet = (props) => {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <h3>{props.animal}</h3>
+      <h3>{props.breed}</h3>
+    </div>
+  )
 }
 
 export default Pet;
--- src/app.js
+++ src/app.js
@@ -1,19 +1,28 @@
 
   import React from 'react';
   import ReactDOM from 'react-dom';
-
   import Pet from './Pet.js'
 
+  // const App = () => {
+  //   return React.createElement (
+  //     "div",
+  //     {},
+  //     [
+  //       React.createElement("h1", {}, "Adopt Me!"),
+  //       React.createElement(Pet),
+  //       React.createElement(Pet),
+  //       React.createElement(Pet),
+  //     ]
+  //   );
+  // };
   const App = () => {
-    return React.createElement (
-      "div",
-      {},
-      [
-        React.createElement("h1", {}, "Adopt Me!"),
-        React.createElement(Pet),
-        React.createElement(Pet),
-        React.createElement(Pet),
-      ]
-    );
+    return (
+            <div>
+              <h1>Adopt Me!</h1>
+              <Pet name="Luna" animal = "Dog" breed = "Bird" />
+              <Pet name="Pepper" animal = "Bird" breed = "Havanese" />
+              <Pet name="Nachito" animal = "Dog" breed = "Havanese" />
+          </div>
+    )
   };
   ReactDOM.render(React.createElement(App), document.getElementById('root'));
