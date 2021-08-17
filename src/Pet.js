217f633d5c6d8d748a8d48dd84ef3ff7af08bd9a
Emmanuel Mota <emmanuelmota@gmail.com>
2021-08-16 19:35:13 -0700

    Adds Drop down with animal options

--- src/Pet.js
+++ src/Pet.js
@@ -1,11 +1,4 @@
 import React from 'react';
-// const Pet = () => {
-//   return React.createElement("div", {}, [
-// React.createElement("h2", {}, "Nacho"),
-// React.createElement("h3", {}, "Krankers"),
-// React.createElement("h3", {}, "Poochi"),
-//   ])
-// }
 
 const Pet = (props) => {
   return (
--- src/app.js
+++ src/app.js
@@ -1,29 +1,15 @@
 
   import React from 'react';
   import ReactDOM from 'react-dom';
-  import Pet from './Pet.js'
+  import SearchParams from './SearchParams';
 
-  // const App = () => {
-  //   return React.createElement (
-  //     "div",
-  //     {},
-  //     [
-  //       React.createElement("h1", {}, "Adopt Me!"),
-  //       React.createElement(Pet),
-  //       React.createElement(Pet),
-  //       React.createElement(Pet),
-  //     ]
-  //   );
-  // };
   const App = () => {
     return (
             <div>
               <h1>Adopt Me!</h1>
-              <Pet name="Luna" animal = "Dog" breed = "Bird" />
-              <Pet name="Pepper" animal = "Bird" breed = "Havanese" />
-              <Pet name="Nachito" animal = "Dog" breed = "Havanese" />
+             <SearchParams />
           </div>
     )
   };
 
-  ReactDOM.render(React.createElement(App), document.getElementById('root'));
+  ReactDOM.render(<App />, document.getElementById('root'));
